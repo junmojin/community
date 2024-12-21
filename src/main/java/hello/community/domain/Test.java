5528143617d298cab0429c02e393b615455661ff
junmojin <o3o060@naver.com>
2024-12-21 15:13:24 +0900

    feat: Test 날짜추가

--- src/main/java/hello/community/domain/Test.java
+++ src/main/java/hello/community/domain/Test.java
@@ -1,15 +1,24 @@
 package hello.community.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate; //LocalDateTime만 하니 2024-12-18T00:16:12.537212 이런 값 나와서 LocalDate로 바꿈
+import java.time.LocalDateTime;
 
 @Entity
 public class Test {
     @Id @GeneratedValue
     private Long id;
+    @Lob
     private String note;
 
+    @CreationTimestamp
+    private LocalDate createdAt;
 
     public Long getId() {
         return id;
@@ -26,4 +35,6 @@ public class Test {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public LocalDate getCreatedAt() {return createdAt; }
 }
--- src/main/java/hello/community/repository/TestRepository.java
+++ src/main/java/hello/community/repository/TestRepository.java
@@ -5,7 +5,7 @@ import hello.community.domain.Test;
 import java.util.List;
 
 public interface TestRepository {
-    void save(Test test);
+    void save(Test test); //Test 객체를 저장하는 기능
 
-    List<Test> findAll();
+    List<Test> findAll(); // 모든 Test 객체를 조회하는 기능, findAll()이 데이터베이스에서 조회하는 기능
 }
